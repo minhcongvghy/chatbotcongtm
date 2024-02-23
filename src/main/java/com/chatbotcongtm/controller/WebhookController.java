e3a33084f058e5b8da753b75337c75dafead133b
DESKTOP-7KR0HP7\Cong <minhcongvghy@gmail.com>
2024-02-23 18:00:08 +0700

    update

--- src/main/java/com/chatbotcongtm/controller/WebhookController.java
+++ src/main/java/com/chatbotcongtm/controller/WebhookController.java
@@ -10,8 +10,6 @@ import com.github.messenger4j.send.NotificationType;
 import com.github.messenger4j.send.message.TextMessage;
 import com.github.messenger4j.send.recipient.IdRecipient;
 import com.github.messenger4j.webhook.event.TextMessageEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +24,6 @@ import static java.util.Optional.of;
 @RestController
 @RequestMapping("/webhook")
 public class WebhookController {
-  private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
   private final Messenger messenger;
 
   @Autowired
@@ -37,12 +34,11 @@ public class WebhookController {
   @GetMapping
   public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
                                               @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken, @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
-    logger.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
+
     try {
       this.messenger.verifyWebhook(mode, verifyToken);
       return ResponseEntity.ok(challenge);
     } catch (MessengerVerificationException e) {
-      logger.warn("Webhook verification failed: {}", e.getMessage());
       return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
   }
@@ -51,28 +47,18 @@ public class WebhookController {
   public ResponseEntity<Void> handleCallback(@RequestBody final String payload, @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) throws MessengerVerificationException {
     this.messenger.onReceiveEvents(payload, of(signature), event -> {
       if (event.isTextMessageEvent()) {
-        try {
-          logger.info("0");
-          handleTextMessageEvent(event.asTextMessageEvent());
-          logger.info("1");
-        } catch (MessengerApiException e) {
-          logger.info("2");
-          e.printStackTrace();
-        } catch (MessengerIOException e) {
-          logger.info("3");
-          e.printStackTrace();
-        }
+        handleTextMessageEvent(event.asTextMessageEvent());
       } else {
         String senderId = event.senderId();
-        sendTextMessageUser(senderId, "Tôi là bot chỉ có thể xử lý tin nhắn văn bản.");
+        sendTextMessageUser(senderId, "Chỉ có thể xử lý tin nhắn văn bản.");
       }
     });
     return ResponseEntity.status(HttpStatus.OK).build();
   }
 
-  private void handleTextMessageEvent(TextMessageEvent event) throws MessengerApiException, MessengerIOException {
+  private void handleTextMessageEvent(TextMessageEvent event) {
     final String senderId = event.senderId();
-    sendTextMessageUser(senderId, "Xin chào! Đây là chatbot được tạo từ ứng dụng Spring Boot");
+    sendTextMessageUser(senderId, "Chat bot");
 
   }
 
@@ -87,11 +73,7 @@ public class WebhookController {
           of(notificationType), empty());
       this.messenger.send(messagePayload);
     } catch (MessengerApiException | MessengerIOException e) {
-      handleSendException(e);
+      e.printStackTrace();
     }
-  }
-
-  private void handleSendException(Exception e) {
-    logger.error("Message could not be sent. An unexpected error occurred.", e);
   }
 }
